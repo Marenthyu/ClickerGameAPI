d92bce81c50f7ece842b35749201f9c0479f8b4f
Peter '"Marenthyu" Fredebold <petersilie93@live.de>
2019-06-11 16:50:36 +0200

    Add /save endpoint

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 let config = require('./config');
 let http = require('http');
-const { Client } = require('pg');
+const {Client} = require('pg');
 let url = require("url");
 config.database.ssl = true;
-const db = new Client(config.database);
+let db = new Client(config.database);
 
 
 db.connect((err) => {
@@ -17,14 +17,188 @@ db.connect((err) => {
     }
 });
 
+function errorWithMessage(res, code, status, err, message) {
+    console.error(err);
+    res.writeHead(code, status, {"Content-Type": "application/json"});
+    let retobject = {success: false, message: message};
+    if (err.hasOwnProperty('detail')) {
+        retobject.detail = err.detail;
+    }
+    res.end(JSON.stringify(retobject));
+}
 
 function saveHandler(req, res, query) {
     console.log("Got request to save");
-    if (req.method === "POST") {
-        res.writeHead(200, "OK", {"Content-Type":"application/json"});
-        res.end('{"success":true, "message":"We got your request"}');
+    if (req.method === "PUT") {
+        let body = '';
+        let aborted = false;
+        req.on('data', function (data) {
+            body += data;
+
+            // Too much POST data, kill the connection!
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            if (body.length > 1e6) {
+                errorWithMessage(res, 413, "POST Data Too Large", "Too much PUT Data", "You sent 1MB or more data, ignoring.");
+                aborted = true;
+            }
+
+        });
+
+        req.on('end', () => {
+            if (aborted) {
+                return
+            }
+            let newData;
+            try {
+                newData = JSON.parse(body);
+            } catch (e) {
+                console.error("Error during put data parsing as JSON:");
+                errorWithMessage(res, 500, "Server Error", e, "Error while parsing your sent data. Aborting.");
+                return
+            }
+            if (!newData.hasOwnProperty('id') || !newData.hasOwnProperty('currency') || !newData.hasOwnProperty('units') || !newData.hasOwnProperty('skills')) {
+                errorWithMessage(res, 400, "Missing Required Data", "Missing required Data field for saving", "Missing required field id, currency, units or skills");
+                return
+            } else {
+                db.query('BEGIN', (err, result) => {
+                    const shouldAbort = (abortErr) => {
+                        if (abortErr) {
+                            db.query("ROLLBACK");
+                            errorWithMessage(res, 500, "Server Error", abortErr, "Error in Transaction during save process. Aborting.");
+                            return true
+                        } else {
+                            return false
+                        }
+                    };
+                    if (!shouldAbort(err)) {
+                        console.log("Transaction acquired successfully. Saving data...");
+                        db.query("SELECT * FROM \"user\" WHERE id = $1 FOR UPDATE", [newData.id], (err, result) => {
+                            if (!shouldAbort(err)) {
+                                if (result.rows.length === 0) {
+                                    db.query("ROLLBACK");
+                                    errorWithMessage(res, 404, "User Not Found")
+                                } else {
+                                    try {
+                                        let units = newData.units;
+                                        let query = "INSERT INTO has_unit(user_id, unit_id, amount) VALUES ";
+                                        let newUnits = [];
+                                        let paramArray = [newData.id];
+                                        let paramcounter = 1;
+                                        let savedUnits = [];
+                                        for (let unit of units) {
+                                            newUnits.push("($1, $" + ++paramcounter + ", $" + ++paramcounter + ")");
+                                            paramArray.push(unit.id);
+                                            paramArray.push(unit.amount);
+                                            savedUnits.push({id: unit.id, amount: unit.amount});
+                                        }
+                                        query = query + (newUnits.join(", ")) + " ON CONFLICT ON CONSTRAINT has_unit_pk DO UPDATE SET amount = excluded.amount";
+                                        console.log("The full query will be:", query);
+                                        console.log("The param Array is:", paramArray);
+                                        db.query(query, paramArray, (err, result) => {
+                                            if (!shouldAbort(err)) {
+                                                try {
+                                                    let skills = newData.skills;
+                                                    let query = "INSERT INTO has_skill(user_id, skill_id, skill_level, last_activation) VALUES ";
+                                                    let newSkills = [];
+                                                    let paramArray = [newData.id];
+                                                    let paramcounter = 1;
+                                                    let savedSkills = [];
+                                                    for (let skill of skills) {
+                                                        newSkills.push("($1, $" + ++paramcounter + ", $" + ++paramcounter + ", $" + ++paramcounter + ")");
+                                                        paramArray.push(skill.id);
+                                                        paramArray.push(skill.skill_level);
+                                                        paramArray.push(skill.last_activation);
+                                                        savedSkills.push({
+                                                            id: skill.id,
+                                                            skill_level: skill.skill_level,
+                                                            last_activation: skill.last_activation
+                                                        });
+                                                    }
+                                                    query = query + (newSkills.join(", ")) + " ON CONFLICT ON CONSTRAINT has_skill_pk DO UPDATE SET last_activation = excluded.last_activation, skill_level = excluded.skill_level";
+                                                    console.log("The full query will be:", query);
+                                                    console.log("The param Array is:", paramArray);
+                                                    db.query(query, paramArray, (err, result) => {
+                                                        if (!shouldAbort(err)) {
+                                                            console.log("Currency: ", newData.currency);
+                                                            db.query("UPDATE \"user\" SET currency = $1 WHERE id = $2", [newData.currency, newData.id], (err, result) => {
+                                                                if (!shouldAbort(err)) {
+                                                                    db.query("COMMIT");
+                                                                    res.writeHead(200, "OK", {"Content-Type": "application/json"});
+                                                                    res.end(JSON.stringify({
+                                                                        success: true,
+                                                                        message: "Found your user, everything updated successfully.",
+                                                                        savedData: {
+                                                                            units: savedUnits,
+                                                                            skills: savedSkills,
+                                                                            user: {currency: newData.currency}
+                                                                        }
+                                                                    }));
+                                                                }
+                                                            });
+
+                                                        }
+                                                    });
+
+                                                } catch (e) {
+                                                    shouldAbort(e);
+                                                }
+
+                                            }
+                                        });
+
+                                    } catch (e) {
+                                        shouldAbort(e);
+                                    }
+
+
+                                }
+                            }
+                        })
+                    }
+                });
+
+
+            }
+
+        });
+    } else if (req.method === "POST") {
+        let body = '';
+        let aborted = false;
+        req.on('data', function (data) {
+            body += data;
+
+            // Too much POST data, kill the connection!
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            if (body.length > 1e6) {
+                errorWithMessage(res, 413, "POST Data Too Large", "Too much POST Data", "You sent 1MB or more data, ignoring.");
+                aborted = true;
+            }
+
+        });
+
+        req.on('end', () => {
+                if (aborted) {
+                    return
+                }
+                let newData;
+                try {
+                    newData = JSON.parse(body);
+                } catch (e) {
+                    console.error("Error during post data parsing as JSON:");
+                    errorWithMessage(res, 500, "Server Error", e, "Error while parsing your sent data. Aborting.");
+                    return
+                }
+                res.writeHead(200, "OK", {"Content-Type": "application/json"});
+                res.end(JSON.stringify({
+                    success: true,
+                    message: "Received your data for a new user. ID will be generated...",
+                    savedData: newData
+                }));
+            }
+        );
+
     } else {
-        res.writeHead(405, "Method Not Allowed", {"Allowed-Methods": "POST"});
+        res.writeHead(405, "Method Not Allowed", {"Allowed-Methods": "POST, PUT"});
         res.end();
 
     }
@@ -34,13 +208,16 @@ function loadHandler(req, res, query) {
     console.log("Got request to load");
     if (req.method === "GET") {
         if (!Object.prototype.hasOwnProperty.call(query, "user")) {
-            res.writeHead(400, "Missing Parameter", {"Content-Type":"application/json"});
-            res.end(JSON.stringify({"success":false, "error":'Missing query parameter "user"'}));
+            res.writeHead(400, "Missing Parameter", {"Content-Type": "application/json"});
+            res.end(JSON.stringify({"success": false, "error": 'Missing query parameter "user"'}));
         } else {
             db.query("SELECT * FROM \"user\" WHERE \"user\".name = $1", [query.user], (err, result) => {
                 if (err) {
-                    res.writeHead(500, "OK", {"Content-Type":"application/json"});
-                    res.end(JSON.stringify({"success":false, "message":"Error during database query. Contact an admin."}));
+                    res.writeHead(500, "OK", {"Content-Type": "application/json"});
+                    res.end(JSON.stringify({
+                        "success": false,
+                        "message": "Error during database query. Contact an admin."
+                    }));
                     console.error(err);
                 } else {
                     result = result.rows;
@@ -48,27 +225,43 @@ function loadHandler(req, res, query) {
                         let userinfo = result[0];
                         db.query("SELECT * FROM has_unit JOIN unit ON has_unit.unit_id = unit.id WHERE has_unit.user_id = $1", [userinfo.id], (err, result) => {
                             if (err) {
-                                res.writeHead(500, "OK", {"Content-Type":"application/json"});
-                                res.end(JSON.stringify({"success":false, "message":"Error during database query. Contact an admin."}));
+                                res.writeHead(500, "OK", {"Content-Type": "application/json"});
+                                res.end(JSON.stringify({
+                                    "success": false,
+                                    "message": "Error during database query. Contact an admin."
+                                }));
                                 console.error(err);
                             } else {
                                 let units = result.rows;
                                 db.query("SELECT * FROM has_skill JOIN skill ON has_skill.skill_id = skill.id WHERE has_skill.user_id = $1", [userinfo.id], (err, result) => {
                                     if (err) {
-                                        res.writeHead(500, "OK", {"Content-Type":"application/json"});
-                                        res.end(JSON.stringify({"success":false, "message":"Error during database query. Contact an admin."}));
+                                        res.writeHead(500, "OK", {"Content-Type": "application/json"});
+                                        res.end(JSON.stringify({
+                                            "success": false,
+                                            "message": "Error during database query. Contact an admin."
+                                        }));
                                         console.error(err);
                                     } else {
                                         let skills = result.rows;
-                                        res.writeHead(200, "OK", {"Content-Type":"application/json"});
-                                        res.end(JSON.stringify({"success":true, "message":"We got your request", "user":userinfo, "units":units, "skills":skills}));
+                                        res.writeHead(200, "OK", {"Content-Type": "application/json"});
+                                        res.end(JSON.stringify({
+                                            "success": true,
+                                            "message": "We got your request",
+                                            "user": userinfo,
+                                            "units": units,
+                                            "skills": skills
+                                        }));
                                     }
                                 });
                             }
                         });
                     } else {
-                        res.writeHead(404, "User Not Found", {"Content-Type":"application/json"});
-                        res.end(JSON.stringify({"success":false, "message":"We got your request, but the requested user was not found.", "user":null}));
+                        res.writeHead(404, "User Not Found", {"Content-Type": "application/json"});
+                        res.end(JSON.stringify({
+                            "success": false,
+                            "message": "We got your request, but the requested user was not found.",
+                            "user": null
+                        }));
                     }
                 }
             });
